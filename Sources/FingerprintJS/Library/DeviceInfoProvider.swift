cd0ef64c54b3f1a1f64dc58c8189647deef2d381
Petr Palata <petr.palata@gmail.com>
2022-04-22 08:45:29 +0200

    Replace hardcoded instances with DI in DeviceInfoProvider

--- Sources/FingerprintJS/Library/DeviceInfoProvider.swift
+++ Sources/FingerprintJS/Library/DeviceInfoProvider.swift
@@ -13,22 +13,47 @@ public protocol DeviceInfoProviding {
 }
 
 public class DeviceInfoProvider {
-    let hardwareInfoHarvester: HardwareInfoHarvesting = HardwareInfoHarvester()
-    let osInfoHarvester: OSInfoHarvesting = OSInfoHarvester()
-    let identifierHarvester: IdentifierHarvesting = IdentifierHarvester()
+    let hardwareInfoHarvester: HardwareInfoHarvesting
+    let osInfoHarvester: OSInfoHarvesting
+    let identifierHarvester: IdentifierHarvesting
 
-    public init() {}
+    public convenience init() {
+        self.init(
+            IdentifierHarvester(),
+            hardwareInfoHarvester: HardwareInfoHarvester(),
+            osInfoHarvester: OSInfoHarvester()
+        )
+    }
+
+    init(
+        _ identifierHarvester: IdentifierHarvesting,
+        hardwareInfoHarvester: HardwareInfoHarvesting,
+        osInfoHarvester: OSInfoHarvesting
+    ) {
+        self.identifierHarvester = identifierHarvester
+        self.hardwareInfoHarvester = hardwareInfoHarvester
+        self.osInfoHarvester = osInfoHarvester
+    }
 }
 
 extension DeviceInfoProvider: DeviceInfoProviding {
 
     public func getDeviceInfo() async -> DeviceInfo {
         return DeviceInfo(
-            vendorIdentifier: identifierHarvester.vendorIdentifier, diskSpace: hardwareInfoHarvester.diskSpaceInfo,
-            screenResolution: hardwareInfoHarvester.displayResolution, deviceType: hardwareInfoHarvester.deviceType,
-            deviceModel: hardwareInfoHarvester.deviceModel, memorySize: nil,
-            physicalMemory: hardwareInfoHarvester.memorySize, cpuCount: hardwareInfoHarvester.cpuCount,
-            osBuild: nil, osVersion: nil, osType: nil, osRelease: nil, kernelVersion: nil)
+            vendorIdentifier: identifierHarvester.vendorIdentifier,
+            diskSpace: hardwareInfoHarvester.diskSpaceInfo,
+            screenResolution: hardwareInfoHarvester.displayResolution,
+            deviceType: hardwareInfoHarvester.deviceType,
+            deviceModel: hardwareInfoHarvester.deviceModel,
+            memorySize: hardwareInfoHarvester.memorySize,
+            physicalMemory: hardwareInfoHarvester.memorySize,
+            cpuCount: hardwareInfoHarvester.cpuCount,
+            osBuild: osInfoHarvester.osBuild,
+            osVersion: osInfoHarvester.osVersion,
+            osType: osInfoHarvester.osType,
+            osRelease: osInfoHarvester.osRelease,
+            kernelVersion: osInfoHarvester.kernelVersion
+        )
     }
 
     public func getDeviceInfo(_ completion: @escaping (DeviceInfo) -> Void) {
