958f1eefa4a98e447559fe640a37019a517b37b1
Petr Palata <petr.palata@fingerprint.com>
2022-09-12 17:54:07 +0200

    Divide SHA256 into CryptoKit/CommonCrypto implementations and cover with a sanity test

--- Sources/FingerprintJS/Hashing/SHA256FingerprintFunction.swift
+++ Sources/FingerprintJS/Hashing/SHA256FingerprintFunction.swift
@@ -11,18 +11,27 @@ import Foundation
 
 class SHA256HashingFunction: FingerprintFunction {
     public func fingerprint(data: Data) -> String {
-        if #available(iOS 13, tvOS 13, *) {
-            let digest = SHA256.hash(data: data)
-            return digest.hexStr
+        if #available(iOS 13.0, tvOS 13.0, *) {
+            return computeSHA256CryptoKit(data)
         } else {
-            var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-            data.withUnsafeBytes {
-                _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
-            }
-            let digest = Data(hash)
-            return digest.map {
-                String(format: "%02hhx", $0)
-            }.joined()
+            return computeSHA256CommonCrypto(data)
         }
+    }
+    
+    @available(iOS 13.0, tvOS 13.0, *)
+    func computeSHA256CryptoKit(_ data: Data) -> String {
+        let digest = SHA256.hash(data: data)
+        return digest.hexStr
+    }
+    
+    func computeSHA256CommonCrypto(_ data: Data) -> String {
+        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
+        }
+        let digest = Data(hash)
+        return digest.map {
+            String(format: "%02hhx", $0)
+        }.joined()
     }
 }
---
+++ Sources/FingerprintJSTests/Tests/SHA256HashingFunctionTests.swift
@@ -0,0 +1,39 @@
+//
+//  SHA256HashingFunctionTests.swift
+//  
+//
+//  Created by Petr Palata on 12.09.2022.
+//
+
+import XCTest
+@testable import FingerprintJS
+
+class SHA256HashingFunctionTests: XCTestCase {
+    private var sut: SHA256HashingFunction!
+
+    override func setUpWithError() throws {
+        sut = SHA256HashingFunction()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    @available(iOS 13.0, tvOS 13.0, *)
+    func testComputesSameHashForBothHashingVersions() {
+        let data = Data(base64Encoded: "dGhpcyBpcyBmaW5nZXJwcmludA==")!
+        
+        let sha256cryptoKit = sut.computeSHA256CryptoKit(data)
+        let sha256commonCrypto = sut.computeSHA256CommonCrypto(data)
+        
+        XCTAssertEqual(sha256cryptoKit, sha256commonCrypto)
+    }
+    
+    func testFingerprintComputesCorrectHashFunction() {
+        let data = Data(base64Encoded: "dGhpcyBpcyBmaW5nZXJwcmludA==")!
+        
+        let fingerprint = sut.fingerprint(data: data)
+        
+        XCTAssertEqual(fingerprint, "d7ffc906ba87c045110c78f34aad06f5259788d7f535b84cf0dd4cdcbf08a436")
+    }
+}
