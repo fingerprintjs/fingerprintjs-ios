bf5104e287bb44bfe32f725c7505d56417bb0205
Michał Gutowski <mgutski@icloud.com>
2022-12-20 17:56:36 +0100

    Remove unused Objective-C header file

--- Sources/FingerprintJS/Harvesters/AppInfoHarvester/Providers/LocaleInfoProviding.swift
+++ Sources/FingerprintJS/Harvesters/AppInfoHarvester/Providers/LocaleInfoProviding.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 protocol LocaleInfoProviding {
     var identifier: String { get }
 }
--- Sources/FingerprintJS/Harvesters/HardwareInfoHarvester/Providers/CPUInfoProviding.swift
+++ Sources/FingerprintJS/Harvesters/HardwareInfoHarvester/Providers/CPUInfoProviding.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 protocol CPUInfoProviding {
     var processorCount: Int { get }
 }
