499f818dabed543d9ff3b3a620d6b63c3b51fcb7
Petr Palata <petr.palata@fingerprint.com>
2023-01-17 18:59:31 +0100

    Cover FingerprintJSVersion convenience methods with UTs

---
+++ Tests/FingerprintJSTests/FingerprintJSVersionTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+
+@testable import FingerprintJS
+
+final class FingerprintJSVersionTests: XCTestCase {
+    func test_givenVersionOne_whenSince_thenReturnsAllVersions() {
+        // given
+        let fromVersion = FingerprintJSVersion.v1
+
+        // when
+        let versions = [FingerprintJSVersion].since(fromVersion)
+
+        // then
+        XCTAssertEqual(versions, .all)
+    }
+
+    func test_givenVersionTwo_whenSince_thenReturnsAllVersionsExceptOne() {
+        // given
+        let fromVersion = FingerprintJSVersion.v2
+
+        // when
+        let versions = [FingerprintJSVersion].since(fromVersion)
+
+        // then
+        XCTAssertEqual(versions, [.v2, .v3, .v4])
+    }
+
+    func test_whenAll_thenReturnsAllVersions() {
+        // when
+        let versions = [FingerprintJSVersion].all
+
+        // then
+        XCTAssertEqual(versions, [.v1, .v2, .v3, .v4])
+    }
+
+    func test_whenLatest_thenReturnsVersionFour() {
+        // when
+        let latest = FingerprintJSVersion.latest
+
+        // then
+        XCTAssertEqual(latest, .v4)
+    }
+}
